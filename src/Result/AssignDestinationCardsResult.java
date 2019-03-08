db6426924a47d6a02699405b104b87d32250ed7c
zacharytaylor <zachtylr04@gmail.com>
2019-03-08 12:03:21 -0700

    Change dest cards response

--- src/Result/AssignDestinationCardsResult.java
+++ src/Result/AssignDestinationCardsResult.java
@@ -1,6 +1,7 @@
 package Result;
 
 import Model.DestinationCard;
+import Model.DestinationCardHand;
 
 import java.util.List;
 
@@ -8,6 +9,7 @@ public class AssignDestinationCardsResult implements iResult {
     private String errorMessage;
     private boolean success;
     private List<DestinationCard> destinationCards;
+    private DestinationCardHand hand;
 
     public String getErrorMessage() {
         return errorMessage;
@@ -31,6 +33,16 @@ public class AssignDestinationCardsResult implements iResult {
 
     public void setDestinationCards(List<DestinationCard> destinationCards) {
         this.destinationCards = destinationCards;
+        hand = new DestinationCardHand();
+        hand.setCards(destinationCards);
+    }
+
+    public DestinationCardHand getHand() {
+        return hand;
+    }
+
+    public void setHand(DestinationCardHand hand) {
+        this.hand = hand;
     }
 }
 
