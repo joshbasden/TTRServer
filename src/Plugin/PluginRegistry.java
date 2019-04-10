b97b9ff2fe29400c941e06ba29c399939c732093
zacharytaylor <zachtylr04@gmail.com>
2019-04-10 11:31:27 -0600

    Refactor Database interface to be in a package so imports work

--- src/Plugin/PluginRegistry.java
+++ src/Plugin/PluginRegistry.java
@@ -1,6 +1,10 @@
 package Plugin;
 
+import Database.Database;
+
 public class PluginRegistry {
 
     public static PluginRegistry instance = new PluginRegistry();
+
+    public Database currentDatabase;
 }
