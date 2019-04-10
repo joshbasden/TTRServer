a61d20bff7d02311050aeb449f678a13761169a1
zacharytaylor <zachtylr04@gmail.com>
2019-04-10 12:19:15 -0600

    Add config file

--- src/Plugin/PluginRegistry.java
+++ src/Plugin/PluginRegistry.java
@@ -2,9 +2,28 @@ package Plugin;
 
 import Database.Database;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class PluginRegistry {
 
     public static PluginRegistry instance = new PluginRegistry();
 
-    public Database currentDatabase;
+    private Database currentDatabase;
+
+    public void registerPlugin(PluginDescriptor descriptor) {
+        try {
+            PrintWriter writer = new PrintWriter("Configuration.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("Config file not found.");
+        }
+    }
+
+    public void setPlugin(PluginDescriptor descriptor) {
+
+    }
+
+    public Database getPlugin() {
+        return currentDatabase;
+    }
 }
