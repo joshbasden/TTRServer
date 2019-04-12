94e98156903fd8ee83998429057210d1489ca646
dmurphy4 <murphy.dallin@byu.edu>
2019-04-12 12:08:43 -0600

    finished mongo stuff

--- MongoDB/src/MongoDB.kt
+++ MongoDB/src/MongoDB.kt
@@ -3,6 +3,7 @@ import java.util.ArrayList
 import com.mongodb.MongoClient
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
+import com.mongodb.client.model.UpdateOptions
 import org.bson.Document
 
 class MongoDB : Database {
@@ -39,7 +40,20 @@ class MongoDB : Database {
     }
 
     override fun getCommandsLength(gameName: String?): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        try {
+            println("Trying to get commands for game $gameName")
+            val db: MongoDatabase = mongoClient!!.getDatabase("TTR")
+            val collection: MongoCollection<Document> = db.getCollection("Commands")
+            val doc = Document("gameName", gameName)
+
+            val commands = collection.find(doc).into(ArrayList<Document>())
+
+            return commands.size
+        }
+        catch(e: Exception) {
+            println("Couldn't get these commands")
+            return -1
+        }
     }
 
     override fun verifyPassword(username: String?, password: String?): Boolean {
@@ -60,8 +74,26 @@ class MongoDB : Database {
         }
     }
 
-    override fun getCommandsForGame(gameName: String?): ArrayList<String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getCommandsForGame(gameName: String?): ArrayList<String>? {
+        try {
+            println("Trying to get commands for game $gameName")
+            val db: MongoDatabase = mongoClient!!.getDatabase("TTR")
+            val collection: MongoCollection<Document> = db.getCollection("Commands")
+            val doc = Document("gameName", gameName)
+
+            val commands = collection.find(doc).into(ArrayList<Document>())
+            val returner = ArrayList<String>()
+
+            for (c in commands) {
+                returner.add(c["command"].toString())
+            }
+
+            return returner
+        }
+        catch(e: Exception) {
+            println("Couldn't get these commands")
+            return null
+        }
     }
 
     override fun addNewUser(username: String?, password: String?): Boolean {
@@ -119,8 +151,11 @@ class MongoDB : Database {
             val filter = Document("gameName", gameName)
             val doc = Document("game", game)
             val setter = Document("\$set", doc)
+            val options = UpdateOptions()
+            options.upsert(true)
+            println(setter.toString())
 
-            collection.updateOne(filter, setter)
+            collection.updateOne(filter, setter, options)
 
             return true
         }
@@ -149,7 +184,20 @@ class MongoDB : Database {
     }
 
     override fun clearCommandsForGame(gameName: String?): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        try {
+            val db = mongoClient!!.getDatabase("TTR")
+            val col = db.getCollection("Commands")
+
+            val doc = Document("gameName", gameName)
+            col.deleteMany(doc)
+
+            return true
+        }
+        catch (e: Exception) {
+            println("Clearing for game $gameName didn't work")
+
+            return false
+        }
     }
 
     override fun initializeSchemas(): Boolean {
@@ -174,10 +222,41 @@ fun main(args: Array<String>) {
     mongoDB.initializeSchemas()
     mongoDB.closeConnection()
     mongoDB.openConnection()
+    mongoDB.addCommand("weirdo", "blah")
+    mongoDB.closeConnection()
+    mongoDB.openConnection()
     mongoDB.addCommand("Dallin", "blah")
     mongoDB.closeConnection()
     mongoDB.openConnection()
-    mongoDB.updateGame("dallinajfdlajsd", "gamergamer")
+    mongoDB.addCommand("Dallin", "b")
+    mongoDB.closeConnection()
+    mongoDB.openConnection()
+    mongoDB.addCommand("Dallin", "bla")
+    mongoDB.closeConnection()
+    mongoDB.openConnection()
+    mongoDB.addCommand("Dallin", "blahasdfasdfs")
+    mongoDB.closeConnection()
+    mongoDB.openConnection()
+    println()
+    println(mongoDB.getCommandsForGame("Dallin").toString())
+    println()
+    mongoDB.closeConnection()
+    mongoDB.openConnection()
+    mongoDB.clearCommandsForGame("Dallin")
+    mongoDB.closeConnection()
+    mongoDB.openConnection()
+    println()
+    println(mongoDB.getCommandsForGame("weirdo").toString())
+    println()
+    mongoDB.closeConnection()
+    mongoDB.openConnection()
+    mongoDB.updateGame("dallinajfdlajsd", "jlasdfjlaksfjdTED!")
+    mongoDB.closeConnection()
+    mongoDB.openConnection()
+    mongoDB.updateGame("dadlajsd", "TED!")
+    mongoDB.closeConnection()
+    mongoDB.openConnection()
+    println(mongoDB.getGames().toString())
     mongoDB.closeConnection()
     mongoDB.openConnection()
     mongoDB.addNewUser("dallin", "boop")
