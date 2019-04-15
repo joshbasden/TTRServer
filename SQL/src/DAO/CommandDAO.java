7a0b2da775433185ec113fd9b4003fddbf804534
Brad <bradley.robert.hales@gmail.com>
2019-04-15 12:12:38 -0600

    SQL updates

--- SQL/src/DAO/CommandDAO.java
+++ SQL/src/DAO/CommandDAO.java
@@ -1,4 +1,151 @@
 package DAO;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 public class CommandDAO {
+    /**
+     * Handles all interaction
+     * with the Person table
+     * in the database
+     */
+    Connection conn;
+
+
+    public CommandDAO(){}
+
+    public boolean addCommand(String gamename, String command) {
+        String sql = "INSERT INTO Commands (GameName, Command) VALUES (?,?);";
+        try{
+            PreparedStatement stmt = null;
+            try {
+                stmt = conn.prepareStatement(sql);
+                stmt.setString(1, gamename);
+                stmt.setString(2, command);
+
+                // update in table
+                stmt.executeUpdate();
+                return true;
+            }
+            finally {
+                if (stmt != null) {
+                    stmt.close();
+                    stmt = null;
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Failed to add Command: " + command + " for " + gamename + ". " + e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean clearCommandsForGame(String gamename) {
+        String sql = "DELETE FROM Commands WHERE GameName = ?;";
+
+        try{
+            PreparedStatement stmt = null;
+            try {
+                stmt = conn.prepareStatement(sql);
+                stmt.setString(1, gamename);
+                stmt.executeUpdate();
+
+                return true;
+            }
+            finally {
+                if (stmt != null) {
+                    stmt.close();
+                    stmt = null;
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Failed to clear commands for " + gamename + ". " + e.getMessage());
+            return false;
+        }
+
+    }
+
+    public int getCommandsLength(String gameName) {
+        ArrayList<String> commandList = null;
+
+        try{
+            commandList = getCommandsForGame(gameName);
+            return commandList.size();
+
+        } catch (Exception e) {
+            System.out.println("Error when trying to get command size for " + gameName + ". " + e.getMessage());
+            return -1;
+        }
+
+    }
+
+    public ArrayList<String> getCommandsForGame(String gameName) {
+
+        String sql = "SELECT Command FROM Commands WHERE GameName = ?";
+
+        ArrayList<String> commandList = null;
+
+        try {
+            PreparedStatement stmt = null;
+            try {
+                System.out.println("attempting to get commands");
+                stmt = conn.prepareStatement(sql);
+                stmt.setString(1, gameName);
+                ResultSet rs = stmt.executeQuery();
+
+                if (rs.isBeforeFirst()){
+                    // loop through result set and add command to list
+                    commandList = new ArrayList<>();
+                    while(rs.next()){
+                        String blob = rs.getString("Command");
+                        commandList.add(blob);
+                    }
+                }
+
+            }
+            finally {
+                if (stmt != null) {
+                    stmt.close();
+                    stmt = null;
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Failed to get Commands for " + gameName);
+            e.printStackTrace();
+        }
+        return commandList;
+    }
+
+    public void clear() throws DataBaseException{
+        /**
+         * clear the person table
+         */
+
+        String sql = "DELETE FROM Persons;";
+        try{
+            Statement stmt = null;
+            try {
+                stmt = conn.createStatement();
+                stmt.executeUpdate(sql);
+
+            }
+            finally {
+                if (stmt != null) {
+                    stmt.close();
+                    stmt = null;
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new DataBaseException("Failed to clear Person table " + e.getMessage());
+        }
+    }
+
+    public void setConn(Connection conn) {
+        this.conn = conn;
+    }
+
+
 }
--- SQL/src/DAO/GameDAO.java
+++ SQL/src/DAO/GameDAO.java
@@ -1,4 +1,106 @@
 package DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class GameDAO {
+    /**
+     * handles all interaction
+     * with the Event table in
+     * the database
+     */
+    Connection conn;
+
+    public GameDAO() {
+    }
+
+    public boolean updateGame(String gameName, String game) {
+        String sql1 = "Select GameName FROM Games WHERE GameName = ?";
+        String sql2 = "UPDATE Games SET Data = ? WHERE GameName = ?;";
+        String sql3 = "INSERT INTO Games VALUES(?,?)";
+        try {
+            PreparedStatement stmt1 = null;
+            PreparedStatement stmt2 = null;
+            try {
+                // check if already exists
+                stmt1 = conn.prepareStatement(sql1);
+                stmt1.setString(1, gameName);
+                ResultSet rs = stmt1.executeQuery();
+
+                if (rs.next()){
+                    stmt2 = conn.prepareStatement(sql2);
+                    stmt2.setString(1, game);
+                    stmt2.setString(2, gameName);
+                    stmt2.executeUpdate();
+                }
+                else{
+                    // not in database
+                    stmt2 = conn.prepareStatement(sql3);
+                    stmt2.setString(1, gameName);
+                    stmt2.setString(2, game);
+                    stmt2.executeUpdate();
+                }
+//
+                return true;
+
+            } finally {
+                if (stmt1 != null) {
+                    stmt1.close();
+                    stmt1 = null;
+                }
+                if (stmt2 != null) {
+                    stmt2.close();
+                    stmt2 = null;
+                }
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Failed to update " + gameName + ". " + e.getMessage());
+            return false;
+        }
+    }
+
+
+
+    public ArrayList<String> getGames() {
+        String sql = "SELECT * FROM Games";
+
+        ArrayList<String> gameList = null;
+
+        try {
+            PreparedStatement stmt = null;
+            try {
+                stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery();
+
+                if (rs.isBeforeFirst()) {
+                    gameList = new ArrayList<>();
+                    // loop through result set and add games to list
+                    while (rs.next()) {
+                        gameList.add(rs.getString("Data"));
+                    }
+                }
+
+            } finally {
+                if (stmt != null) {
+                    stmt.close();
+                    stmt = null;
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error getting all the games. " + e.getMessage());
+        }
+
+        return gameList;
+
+    }
+    public void setConn(Connection conn) {
+        this.conn = conn;
+    }
+
 }
