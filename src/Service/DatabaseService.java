737585416d19dc222996373720fc12fb46430e33
zacharytaylor <zachtylr04@gmail.com>
2019-04-15 14:09:29 -0600

    Add open and close connections to database service

--- src/Service/DatabaseService.java
+++ src/Service/DatabaseService.java
@@ -12,34 +12,122 @@ public class DatabaseService {
     private Database database = PluginRegistry.instance.getDatabase();
 
     public boolean addNewUser(String username, String password) {
-        return database.addNewUser(username, password);
+        try {
+            database.openConnection();
+            boolean success = database.addNewUser(username, password);
+            database.closeConnection(success);
+            return success;
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                database.closeConnection(false);
+            } catch (Exception f) { return false; }
+            return false;
+        }
     }
 
     public boolean updateGame(String gameName, Game game) {
-        return database.updateGame(gameName, new Gson().toJson(game));
+        try {
+            database.openConnection();
+            boolean success = database.updateGame(gameName, new Gson().toJson(game));
+            database.closeConnection(success);
+            return success;
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                database.closeConnection(false);
+            } catch (Exception f) { return false; }
+            return false;
+        }
     }
 
     public boolean addCommand(String gameName, iServerCommand command) {
-        return database.addCommand(gameName, new Gson().toJson(command));
+        try {
+            database.openConnection();
+            boolean success = database.addCommand(gameName, new Gson().toJson(command));
+            database.closeConnection(success);
+            return success;
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                database.closeConnection(false);
+            } catch (Exception f) { return false; }
+            return false;
+        }
     }
 
     public boolean clearCommandsForGame(String gameName) {
-        return database.clearCommandsForGame(gameName);
+        try {
+            database.openConnection();
+            boolean success = database.clearCommandsForGame(gameName);
+            database.closeConnection(success);
+            return success;
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                database.closeConnection(false);
+            } catch (Exception f) { return false; }
+            return false;
+        }
     }
 
     public int getCommandsLength(String gameName) {
-        return database.getCommandsLength(gameName);
+        try {
+            database.openConnection();
+            int numCommands = database.getCommandsLength(gameName);
+            database.closeConnection(true);
+            return numCommands;
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                database.closeConnection(false);
+            } catch (Exception f) { return -1; }
+            return -1;
+        }
     }
 
     public boolean verifyPassword(String username, String password) {
-        return database.verifyPassword(username, password);
+        try {
+            database.openConnection();
+            boolean success = database.verifyPassword(username, password);
+            database.closeConnection(success);
+            return success;
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                database.closeConnection(false);
+            } catch (Exception f) { return false; }
+            return false;
+        }
     }
 
     public ArrayList<String> getGames() {
-        return database.getGames();
+        try {
+            database.openConnection();
+            ArrayList<String> games = database.getGames();
+            database.closeConnection(true);
+            return games;
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                database.closeConnection(false);
+            } catch (Exception f) { return new ArrayList<>(); }
+            return new ArrayList<>();
+        }
     }
 
     public ArrayList<String> getCommandsForGame(String gameName) {
-        return database.getCommandsForGame(gameName);
+        try {
+            database.openConnection();
+            ArrayList<String> commands = database.getCommandsForGame(gameName);
+            database.closeConnection(true);
+            return commands;
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                database.closeConnection(false);
+            } catch (Exception f) { return new ArrayList<>(); }
+            return new ArrayList<>();
+        }
     }
 }
