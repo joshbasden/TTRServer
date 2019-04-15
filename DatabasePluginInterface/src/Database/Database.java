5caf748b2a66c867250a612d53fc0940e23800f1
zacharytaylor <zachtylr04@gmail.com>
2019-04-15 12:49:49 -0600

    Remake database plugin interface jar

--- DatabasePluginInterface/src/Database/Database.java
+++ DatabasePluginInterface/src/Database/Database.java
@@ -3,15 +3,15 @@ package Database;
 import java.util.ArrayList;
 
 public interface Database {
-    boolean addNewUser(String username, String password);
-    boolean updateGame(String gameName, String game);
-    boolean addCommand(String gameName, String command);
-    boolean clearCommandsForGame(String gameName);
-    int getCommandsLength(String gameName);
-    boolean verifyPassword(String username, String password);
-    boolean openConnection();
-    boolean closeConnection();
-    boolean initializeSchemas();
-    ArrayList<String> getGames();
-    ArrayList<String> getCommandsForGame(String gameName);
+    boolean addNewUser(String username, String password) throws DatabaseException;
+    boolean updateGame(String gameName, String game) throws DatabaseException;
+    boolean addCommand(String gameName, String command) throws DatabaseException;
+    boolean clearCommandsForGame(String gameName) throws DatabaseException;
+    int getCommandsLength(String gameName) throws DatabaseException;
+    boolean verifyPassword(String username, String password) throws DatabaseException;
+    boolean openConnection() throws DatabaseException;
+    boolean closeConnection(boolean success) throws DatabaseException;
+    boolean initializeSchemas() throws DatabaseException;
+    ArrayList<String> getGames() throws DatabaseException;
+    ArrayList<String> getCommandsForGame(String gameName) throws DatabaseException;
 }
---
+++ DatabasePluginInterface/src/Database/DatabaseException.java
@@ -0,0 +1,7 @@
+package Database;
+
+class DatabaseException extends Exception {
+    public DatabaseException(String errorMessage, Throwable err) {
+        super(errorMessage, err);
+    }
+}
